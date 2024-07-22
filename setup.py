d09724a41213eb7180f985cbba28ed356d9f2b11
Georgios Kafanas <georgios.kafanas@uni.lu>
2024-07-22 16:35:11 +0200

    Update dependencies for Conda build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="smorfinder",
-    version='1.0.0',
+    version='1.0.0-ulhpc',
     description='A command line tool to identify and annotate small proteins in genomes and metagenomes.',
     url='https://github.com/bhattlab/SmORFinder',
     author="Matt Durrant",
@@ -11,10 +11,11 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        'numpy==1.19.5',
         'click==7.0',
         'biopython==1.76',
-        'tensorflow==2.3.1',
-        'wget==3.2'
+        'tensorflow==2.4.3',
+        'python-wget==3.2'
     ],
     zip_safe=False,
     entry_points = {
